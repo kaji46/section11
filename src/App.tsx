ab9ab3a1340a43a2dc63ddce49fb043d7ae7a1b3
Naoto Kajimura <naotokajimura@gmail.com>
2021-07-24 15:28:48 +0900

    section 11 finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
+import axios from "axios";
+import { UserCard } from "./components/UserCard";
 import "./styles.css";
+import { UserProfile } from "./types/userProfile";
+import { User } from "./types/api/user";
+import { useState } from "react";
+import { useAllUsers } from "./hooks/useAllUsers";
+// const user: UserProfile = {
+//   id: 1,
+//   name: "name",
+//   email: "aaa@aa",
+//   address: "aaa"
+// };
 
 export default function App() {
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+  const onClickFetchData = () => getUsers();
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={onClickFetchData}>データ取得</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {console.log("userProfile")}
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
     </div>
   );
 }
